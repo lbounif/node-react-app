0c4e9ad14d57ccf0a84e8b7063eae7ed5496fad9
Bounif Lynda <lynda.bounif@imadrassa.com>
2020-05-28 18:02:32 +0100

    add routes

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+require('./models/book')
+
 const app = express()
 
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`)
 
 app.use(bodyParser.json())
+require('./routes/bookRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
---
+++ routes/bookRoutes.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const Book = mongoose.model('books')
+
+module.exports = (app) => {
+
+  app.get(`/api/book`, async (req, res) => {
+    let books = await Book.find()
+    return res.status(200).send(books)
+  })
+
+  app.post(`/api/book`, async (req, res) => {
+    let book = await Book.create(req.body)
+    return res.status(201).send({
+      error: false,
+      book
+    })
+  })
+
+  app.put(`/api/book/:id`, async (req, res) => {
+    const {id} = req.params
+
+    let book = await Book.findByIdAndUpdate(id, req.body)
+
+    return res.status(202).send({
+      error: false,
+      book
+    })
+
+  })
+
+  app.delete(`/api/book/:id`, async (req, res) => {
+    const {id} = req.params
+
+    let book = await Book.findByIdAndDelete(id)
+
+    return res.status(202).send({
+      error: false,
+      book
+    })
+
+  })
+
+}
